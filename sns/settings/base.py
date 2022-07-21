ceaf2353d31cce0e3c265d3ece54d789b0c3fcda
jay <jaewhan232@naver.com>
2022-07-21 21:36:47 +0900

    Chore - simpleJWT 추가

--- sns/settings/base.py
+++ sns/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
+from datetime import timedelta
 
 import os, json
 
@@ -43,11 +44,12 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # Application definition
 APPS = [
     'users',
-    'posts'
+    'posts',
 ]
 
 THIRD_PARTIES = [
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 INSTALLED_APPS = [
@@ -104,6 +106,22 @@ DATABASES = {
 
 # AUTH_USER_MODEL
 AUTH_USER_MODEL = 'users.USER'
+
+#REST_FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+# JWT_AUTH settings
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
