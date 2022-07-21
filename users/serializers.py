591aee2cf915dfda3d50aa91fb3d4245732a328d
jay <jaewhan232@naver.com>
2022-07-21 21:33:30 +0900

    Feat - UserSignupSerializer create 메소드 추가 User 모델의 create_user 메소드 호출

--- users/serializers.py
+++ users/serializers.py
@@ -6,6 +6,9 @@ class CreateUserSerializer(ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ('email', 'password') 
+    
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
 
 class BaseUserSerializer(ModelSerializer):
     class Meta:
