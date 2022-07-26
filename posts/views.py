8d9b086ec7f7adfc9bf51fcd92139b69fda45d5c
jay <jaewhan232@naver.com>
2022-07-26 15:45:45 +0900

    Feat - 게시글 등록 기능 구현 IsAuthenticated permission class -> 로그인된 사용자만 게시글 작성 권한 부여

--- posts/views.py
+++ posts/views.py
@@ -10,6 +10,7 @@ from users.serializers import BaseUserSerializer
 
 from .serializers import *
 from .models import *
+from .utils.permissions import *
 
 
 # Create your views here.
@@ -21,3 +22,15 @@ class PostListAPI(APIView):
         post_queryset = Post.objects.all()
         serializer = PostSerializer(post_queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    @transaction.atomic()
+    def post(self, request):
+        permission_classes = [IsAuthenticated]
+        
+        posted_user = get_user_model().objects.get(pk=request.user.id)
+        new_post = Post.objects.create(title=request.data['title'], content=request.data['content'], user=posted_user)
+        for hashtag in request.data['hashtag']:
+            new_tag, is_created = Hashtag.objects.get_or_create(tag=hashtag)
+            new_post.hashtag.add(new_tag)
+        new_post.save()
+        serializer = PostSerializer(new_post)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
