b9861147e0d46e5850982c6332a8489791a9c1ee
jay <jaewhan232@naver.com>
2022-07-28 11:28:54 +0900

    Feat - 게시글 권한 에러 메세지 생성

--- posts/utils/permissions.py
+++ posts/utils/permissions.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import get_user_model
+
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerOrReadOnly(BasePermission):
-
+    message = "해당 게시글의 권한이 없습니다."
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
         else:
             return obj.user == request.user
+        
+    
+    
